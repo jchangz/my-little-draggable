ee5bda8dba8c27035fce4d805fbe3a6d5c39c2ee
Jian Chang <45157140+jchangz@users.noreply.github.com>
2023-06-28 03:10:25 -0600

    feat: initialize app with useDrag
    * calculate index, current row and column of selected item
    
    * calculate offset from top for each row

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { useSprings, a } from "@react-spring/web";
+import { useDrag } from "@use-gesture/react";
+import { useGridProps } from "./helpers/useGridProps";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null);
+  const {
+    gridColumnWidth,
+    gridOffsetFromTop,
+    gridRowHeights,
+    gridGap,
+    maxRows,
+    maxCols,
+  } = useGridProps(containerRef);
+  const [springs, api] = useSprings(6, () => ({
+    x: 0,
+    y: 0,
+    opacity: 0,
+    shadow: 0,
+    zIndex: 0,
+  }));
+
+  let order = [1, 2, 3, 4, 5, 6].map((...[, index]) => index);
+
+  const getNewMaxHeights = (order: Array<number>) => {
+    const rowHeightObj: RowData = {};
+
+    order.forEach((val, i) => {
+      const rowNum = Math.ceil((i + 1) / maxCols);
+      if (!rowHeightObj[rowNum])
+        rowHeightObj[rowNum] = gridRowHeights.current[val];
+      else {
+        if (gridRowHeights.current[val] > rowHeightObj[rowNum]) {
+          rowHeightObj[rowNum] = gridRowHeights.current[val];
+        }
+      }
+    });
+
+    const arr: Array<number> = Object.values(rowHeightObj);
+    return arr;
+  };
+
+  let currentMaxHeightPerRow: Array<number> = [];
+  let currentIndexPosition = 0;
+  const offsetTopOfRows = new Array(maxRows.current).fill(
+    gridOffsetFromTop.current
+  );
+
+  const bind = useDrag(
+    ({ args: [originalIndex], down, active, first, movement: [mx, my] }) => {
+      if (down && mx === 0 && my === 0) thisIndex = originalIndex;
+
+      if (first) {
+        currentIndexPosition = order.indexOf(originalIndex);
+        currentMaxHeightPerRow = getNewMaxHeights(order);
+      }
+
+      const currentRow = Math.floor(currentIndexPosition / maxCols);
+      const currentCol = currentIndexPosition % maxCols;
+
+      for (let i = 1; i < maxRows.current; i += 1) {
+        offsetTopOfRows[i] =
+          offsetTopOfRows[i - 1] + currentMaxHeightPerRow[i - 1];
+      }
+    },
+    {
+      bounds: containerRef,
+      preventDefault: true,
+    }
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bodycon" ref={containerRef}>
+        {springs.map(({ x, y, zIndex, shadow }, i) => (
+          <a.div
+            {...bind(i)}
+            className={`item-${i}`}
+            style={{
+              x,
+              y,
+              zIndex,
+              boxShadow: shadow.to(
+                (s) => `rgba(0, 0, 0, 0.5) 0px ${s}px ${2 * s}px 0px`
+              ),
+            }}
+          >
+            <div className="drag-item">
+              <div className="bg-red"></div>
+            </div>
+          </a.div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
